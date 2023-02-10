816289a1dcd61ed21f81e97f9ea44c18f1e4381f
dva <dvalvu@qq.com>
2023-02-10 13:39:11 +0800

    ''

--- g.py
+++ g.py
@@ -1,32 +1,48 @@
 import torch
 
 
-dim_x = 2
-dim_y = 1
+dim_x = 2+1
+dim_y = 1+1
 
-dim_w = [4]
+dim_w = [4+1]
 lw = list()
 lx = list()
 
 x = torch.randn(dim_x, 1)
+x[dim_x-1]=1
 lx.append(x)
 
 w = torch.randn(dim_w[0], dim_x, requires_grad=True)
 lw.append(w)
 
-x1 = torch.matmul(lw[0],lx[0])
+x = torch.matmul(lw[0],lx[0])
+torch.clamp(x,min=0.0)
+print(x)
+lx.append(x)
 
 # w1
 w = torch.randn(dim_y,dim_w[0],requires_grad = True)
-x = torch.matmul(w,x1)
+lw.append(w)
 
-c = torch.sum(x)
+x = torch.matmul(w,lx[-1])
+torch.clamp(x,min=0.0)
 lx.append(x)
 
 
+c = torch.sum(lx[-1])
+
+
+for i,ele in enumerate(lx):
+    print(ele)
 
 
 
 c.backward()
-print(lw[0].grad)
-print(lw[1].grad)
+# print(lw[0].grad)
+# print(lw[1].grad)
+
+
+a = torch.randn(5)
+print(a)
+torch.clamp(a,min=0.0)
+print(a)
