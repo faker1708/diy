dc78dedaf52f2001394dd5b909b1b553bffbed35
dva <dvalvu@qq.com>
2023-02-10 13:25:39 +0800

    ''

--- g.py
+++ g.py
@@ -1,15 +1,24 @@
 import torch
 
 
+x0 = torch.randn(2, 1)
 
 
-w = torch.randn(4, 2, requires_grad=True)
-x0 = torch.randn(4, 2)
+w0 = torch.randn(4, 2, requires_grad=True)
 
 
-x1 = w*x0
-
-print(x1)
+x1 = torch.matmul(w0,x0)
 
 
+w1 = torch.randn(1,4,requires_grad = True)
 
+x2 = torch.matmul(w1,x1)
+
+c = torch.sum(x2)
+
+
+
+
+c.backward()
+print(w0.grad)
+print(w1.grad)
