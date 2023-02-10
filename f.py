55971026a09f6d8b781d08613572015a93a95e6a
dva <dvalvu@qq.com>
2023-02-10 13:21:09 +0800

    ''

--- f.py
+++ f.py
@@ -1,26 +1,40 @@
 # numpy
 import numpy as np
-np.random.seed(0)
+import torch
+
+
+
+np.random.seed(777)
 
 N, D = 3, 4
+rb = 7
 
-x = np.random.randn(N, D)
-y = np.random.randn(N, D)
-z = np.random.randn(N, D)
+# x = np.random.randn(N, D)
+# y = np.random.randn(rb, D)
+# z = np.random.randn(N, D)
 
-a = x * y
-b = a + z
-c = np.sum(b)
+# a = x * y
+# b = a + z
+# c = np.sum(b)
 
 
-import torch
 x = torch.randn(N, D, requires_grad=True)
 y = torch.randn(N, D)
 z = torch.randn(N, D)
 
+print(x)
+print(y)
+
+
 a = x * y
 b = a + z
 c = torch.sum(b)
+
+
+print(a)
+
+
+print("c",c)
 
 c.backward()
 print(x.grad)
---
+++ g.py
@@ -0,0 +1,8 @@
+import torch
+
+
+
+
+x = torch.randn(4, 2, requires_grad=True)
+
+print(x)
